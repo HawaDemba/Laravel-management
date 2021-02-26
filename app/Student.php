64a5fff1449d684711e1aa9355da9d2d68930b19
HawaDemba <hawadembacoulikou@gmail.com>
2021-02-26 11:15:05 +0000

    first commit

---
+++ app/Student.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $fillable = [
+        'cne', 'firstName', 'secondName', 'age', 'speciality'
+    ];
+}
